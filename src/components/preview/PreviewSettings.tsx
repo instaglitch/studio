57dbd6223bdbdf17d52861ecbad2b79d8e76826a
Mat Sz <contact@matsz.dev>
2021-06-13 17:48:15 +0200

    replaced react-dat-gui with react-var-ui

--- src/components/preview/PreviewSettings.tsx
+++ src/components/preview/PreviewSettings.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { toJS } from 'mobx';
-import DatGui, {
-  DatNumber,
-  DatBoolean,
-  DatFolder,
-  DatColor,
-  DatSelect,
-} from 'react-dat-gui';
+import {
+  VarUI,
+  VarSlider,
+  VarColor,
+  VarToggle,
+  VarXY,
+  VarSelect,
+} from 'react-var-ui';
 
 import { useProjectStore } from '../../ProjectStore';
 import { FilterSettingType } from '../../types';
-
-declare module 'react-dat-gui' {
-  interface DatSelectProps extends DatChangableFieldProps {
-    options: any[];
-    optionLabels: any[];
-  }
-}
 
 export const PreviewSettings: React.FC = observer(() => {
   const projectStore = useProjectStore();
 
   return (
-    <DatGui
-      data={toJS(projectStore.settingValues)}
-      onUpdate={data => {
-        for (const setting of projectStore.settings) {
+    <VarUI
+      values={toJS(projectStore.settingValues)}
+      updateValues={(data: any) => {
+        for (const setting of projectStore.settings!) {
           if (setting.type === FilterSettingType.SELECT) {
             data[setting.key] = parseInt(data[setting.key]);
           }
@@ -46,18 +40,19 @@ export const PreviewSettings: React.FC = observer(() => {
         switch (setting.type) {
           case FilterSettingType.INTEGER:
             return (
-              <DatNumber
+              <VarSlider
                 min={setting.minValue ?? -1}
                 max={setting.maxValue ?? 1}
                 step={setting.step ?? 1}
                 path={setting.key}
                 label={name}
                 key={setting.id}
+                integer
               />
             );
           case FilterSettingType.FLOAT:
             return (
-              <DatNumber
+              <VarSlider
                 min={setting.minValue ?? -1}
                 max={setting.maxValue ?? 1}
                 step={setting.step ?? 0.01}
@@ -68,40 +63,26 @@ export const PreviewSettings: React.FC = observer(() => {
             );
           case FilterSettingType.COLOR:
             return (
-              <DatColor path={setting.key} label={name} key={setting.id} />
+              <VarColor path={setting.key} label={name} key={setting.id} />
             );
           case FilterSettingType.BOOLEAN:
             return (
-              <DatBoolean path={setting.key} label={name} key={setting.id} />
+              <VarToggle path={setting.key} label={name} key={setting.id} />
             );
           case FilterSettingType.OFFSET:
-            return (
-              <DatFolder title={name} closed={false} key={setting.id}>
-                <DatNumber
-                  min={-1}
-                  max={1}
-                  step={0.01}
-                  path={setting.key + '.0'}
-                  label={'X'}
-                />
-                <DatNumber
-                  min={-1}
-                  max={1}
-                  step={0.01}
-                  path={setting.key + '.1'}
-                  label={'Y'}
-                />
-              </DatFolder>
-            );
+            return <VarXY label={name} path={setting.key} key={setting.id} />;
           case FilterSettingType.SELECT:
             return (
-              <DatSelect
+              <VarSelect
                 path={setting.key}
                 label={name}
                 key={setting.id}
-                options={setting.selectValues?.map(value => value.value) || []}
-                optionLabels={
-                  setting.selectValues?.map(value => value.name) || []
+                options={
+                  setting.selectValues?.map(value => ({
+                    key: value.id,
+                    label: value.name,
+                    value: value.value,
+                  })) || []
                 }
               />
             );
@@ -109,6 +90,6 @@ export const PreviewSettings: React.FC = observer(() => {
 
         return null;
       })}
-    </DatGui>
+    </VarUI>
   );
 });
