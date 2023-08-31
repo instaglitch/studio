9f642c027bfd4b8e902f112946724345f03db45d
Mat Sz <contact@matsz.dev>
2023-08-31 18:08:50 +0200

    migrate to vite

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => ({
+  base: '',
+  resolve:
+    mode === 'production'
+      ? {
+          // Enables MobX production build
+          mainFields: ['jsnext:main', 'module', 'main'],
+        }
+      : undefined,
+  build: {
+    outDir: './build',
+  },
+  define: {
+    global: 'window',
+  },
+  plugins: [react()],
+}));
